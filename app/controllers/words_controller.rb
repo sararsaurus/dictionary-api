22d1c9cb0399341d02ae0c7578471768327f6061
Sara Raley <sara.m.raley@gmail.com>
2022-12-28 09:47:02 -0700

    fixes params path in words controller index

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,20 +2,18 @@ class WordsController < ApplicationController
   def index
 
     ## the below works
-    response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
+    # response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
 
-    definition = response.parse(:json)[0]["meanings"]
-    render json: definition
+    # definition = response.parse(:json)[0]["meanings"]
+    # render json: definition
 
     ## the below doesn't
     if params[:search]
-      response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/#{params}")
-      definition = response.parse(:json)[0]["meanings"]
-      render json: definition
+      response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/#{params[:search]}")
     else
       response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
-      definition = response.parse(:json)[0]["meanings"]
-      render json: definition
     end
+    definition = response.parse(:json)[0]["meanings"]
+    render json: definition
   end
 end
