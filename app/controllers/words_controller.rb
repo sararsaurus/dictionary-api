47e72ea3de4e39fd7d83a241bbc0c18ad725766e
Sara Raley <sara.m.raley@gmail.com>
2022-12-27 17:39:14 -0700

    adds http gem and index to access the meaning of 'hello'

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,2 +1,21 @@
 class WordsController < ApplicationController
+  def index
+    response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
+
+    meanings = response.parse(:json)[0]["meanings"]
+    render json: meanings
+
+    # if params[:search]
+    #   response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/#{params}")
+
+    #   data = response.parse(:json)
+    #   articles = data["articles"]
+    # else
+    #   response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
+
+    #   data = response.parse(:json)
+    #   articles = data["articles"]
+    # end
+    # render json: articles
+  end
 end
