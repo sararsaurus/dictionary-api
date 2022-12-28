89d2cf23bff196fb6a892e2d882d682ffa22f5d5
Sara Raley <sara.m.raley@gmail.com>
2022-12-27 18:02:35 -0700

    attempts incorrect query param conditional

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,21 +1,21 @@
 class WordsController < ApplicationController
   def index
+
+    ## the below works
     response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
 
-    meanings = response.parse(:json)[0]["meanings"]
-    render json: meanings
+    definition = response.parse(:json)[0]["meanings"]
+    render json: definition
 
-    # if params[:search]
-    #   response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/#{params}")
-
-    #   data = response.parse(:json)
-    #   articles = data["articles"]
-    # else
-    #   response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
-
-    #   data = response.parse(:json)
-    #   articles = data["articles"]
-    # end
-    # render json: articles
+    ## the below doesn't
+    if params[:search]
+      response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/#{params}")
+      definition = response.parse(:json)[0]["meanings"]
+      render json: definition
+    else
+      response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
+      definition = response.parse(:json)[0]["meanings"]
+      render json: definition
+    end
   end
 end
