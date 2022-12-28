183ca5e344cdab99b4ea1a06e7d8d6706bc338aa
Sara Raley <sara.m.raley@gmail.com>
2022-12-28 09:47:30 -0700

    cleans up comments

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,13 +1,12 @@
 class WordsController < ApplicationController
   def index
 
-    ## the below works
+    ## basic call
     # response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
 
     # definition = response.parse(:json)[0]["meanings"]
     # render json: definition
 
-    ## the below doesn't
     if params[:search]
       response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/#{params[:search]}")
     else
