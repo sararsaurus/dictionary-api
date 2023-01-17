37c5398fc176fb64b9f1aebb62993a7221d8db05
Sara Raley <sara.m.raley@gmail.com>
2023-01-17 15:34:00 -0700

    updates cors

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,12 +1,5 @@
 class WordsController < ApplicationController
   def index
-
-    ## basic call
-    # response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
-
-    # definition = response.parse(:json)[0]["meanings"]
-    # render json: definition
-
     if params[:search]
       response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/#{params[:search]}")
     else
@@ -16,3 +9,9 @@ class WordsController < ApplicationController
     render json: definition
   end
 end
+
+## basic call
+# response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
+
+# definition = response.parse(:json)[0]["meanings"]
+# render json: definition
