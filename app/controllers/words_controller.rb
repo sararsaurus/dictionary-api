6e169b3c404985f2ecb61c99fcfc80e71b9c2b82
Sara Raley <sara.m.raley@gmail.com>
2023-01-17 16:52:45 -0700

    adds cors gem

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,14 +1,15 @@
 class WordsController < ApplicationController
   def index
-    if params[:search]
-      response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/#{params[:search]}")
-    else
-      response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
-    end
-    definition = response.parse(:json)[0]["meanings"]
+    response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
+
+    definition = response.parse(:json)
     render json: definition
   end
 end
+
+# if params[:search]
+#   response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/#{params[:search]}")
+# else
 
 ## basic call
 # response = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
